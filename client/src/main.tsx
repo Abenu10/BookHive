9ce8ac68e4441c6e6a8a8dc9fbd16582bdd09a76
Abenu10 <abmlengineer@gmail.com>
2024-08-04 18:55:11 +0300

    feat: Integrate React Router and Redux into the application

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,18 @@
-import { jsxDEV } from "react/jsx-dev-runtime";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.tsx';
+import './index.css';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './redux/store.ts';
+// import {store} from './state/store';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
